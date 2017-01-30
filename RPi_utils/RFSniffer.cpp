aaf8455ae26426871798d8ac15a375861b7e0f41
Xavier Naveira <xavier.naveira@ooyala.com>
2017-01-30 14:14:42 +0100

    Fixed type missmatch in printf

--- RPi_utils/RFSniffer.cpp
+++ RPi_utils/RFSniffer.cpp
@@ -47,7 +47,7 @@ int main(int argc, char *argv[]) {
           printf("Unknown encoding\n");
         } else {    
    
-          printf("Received %i\n", mySwitch.getReceivedValue() );
+          printf("Received %lu\n", mySwitch.getReceivedValue() );
         }
     
         mySwitch.resetAvailable();
